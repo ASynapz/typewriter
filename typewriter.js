c23134d59adbf2ae69a077f13af253f109ac4bd5
Alan Wu <59207578+ASynapz@users.noreply.github.com>
2020-04-07 12:11:56 -0700

    Add files via upload

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "hello there from lighthouse labs";
+
+let idx = 0;
+for (const char of sentence) {
+  idx += 1000;
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, idx);
+}
+console.log("\n");
